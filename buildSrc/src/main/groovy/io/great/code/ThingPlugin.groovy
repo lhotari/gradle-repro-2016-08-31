bb74c51f7145e2fab540d49ea5f2496a63b12dfa
Lari Hotari <lari@hotari.net>
2016-09-05 16:38:48 +0300

    Make ThingPlugin more like the reported one

--- buildSrc/src/main/groovy/io/great/code/ThingPlugin.groovy
+++ buildSrc/src/main/groovy/io/great/code/ThingPlugin.groovy
@@ -6,6 +6,35 @@ import org.gradle.api.Project
 class ThingPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.convention.plugins.put('thing', new ThingPluginConvention())
+        project.convention.plugins.generalUtilities = new ThingPluginConvention(project)
+    }
+}
+
+class ThingPluginConvention {
+    private final Project project
+
+    ThingPluginConvention(Project project) {
+        this.project = project
+    }
+
+    // from https://discuss.gradle.org/t/strange-class-not-found-error-for-closure/18982
+
+    // Expected version format is a sequence of numbers, separated by periods.  E.g. 1.2.3.4.5
+    private List<Integer> parseThingVersion(String version) {
+        println "Parsing as version: $version"
+        String[] sarr = version.split(/\./)
+        Integer[] narr = sarr.collect() { String s ->  // <<< THIS CLOSURE IS NOT FOUND
+            if (s == null) {
+                return 0
+            }
+            // Pick leading digits with "\d+" to handle 2.4-20150222230019+0000
+            def n = s =~ /\d+/
+            if (n) {
+                return n[0].toInteger()
+            } else {
+                return 0
+            }
+        }
+        return narr.toList()
     }
 }
